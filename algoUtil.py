91961ef54177e1cb0745115c3a42840941d721a2
Alejandro Pogi <knggrim@gmail.com>
2024-05-09 08:50:26 +0800

    added taskWaitingTime and avg to  util module and added shift and timeExecuted attribute to task module

--- algoUtil.py
+++ algoUtil.py
@@ -6,5 +6,26 @@ class algoUtil:
             totalBurst += task.cpuBurst
         return totalBurst
     
+    def taskWaitingTime(self, waitingT,arrivalT):
+        #waiting time = waiting time - arrival time
+        return waitingT - arrivalT
+    
+    def avg(self,list):
+        num = 0
+        for i in list:
+            num += i
+        return num/len(list)
+
+
 if __name__ == '__main__':
-    pass
+    algo = algoUtil()
+    task = [[8,3],[3,1],[2,1]]
+    finalList = []
+    for t in task:
+        finalList.append(algo.taskWaitingTime(t[0],t[1]))
+    
+    print(algo.avg(finalList))
+        
+
+
+    
--- task.py
+++ task.py
@@ -4,6 +4,18 @@ class Task:
         self.id = id
         self.arrivalTime = arrivalTime
         self.cpuBurst = cpuBurst
+        #time when executed can be 1 or many depending on the process
+        #preemptive only has 1 non-preemptive has 1 or more
+        self.timeExecuted = []
+        #time shifted value can be 1 or more depending on the process 
+        #preemptive only has 1 non-preemptive has 1 or more
+        self.shift = []
+
+    def addTimeExecuted(self,T):
+        self.timeExecuted.append(T)
+        
+    def addShift(self,T):
+        self.shift.append(T)
         
     def __str__(self) -> str:
         return f"""ID: {self.id}
