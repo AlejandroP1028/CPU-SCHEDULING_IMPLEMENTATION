9719833bd42987c7359f68020802a85dc29faa47
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-12 17:16:55 +0800

    added waiting time util for preemptive srtf and cleaned some fucntions

--- test.py
+++ test.py
@@ -31,11 +31,10 @@ class Task:
         self.arrival_time = arrival_time
         self.cpu_burst = cpu_burst
         self.cpu_burst_needed = cpu_burst
-        self.time_executed: list[int] = []  # Time when executed can be 1 or many depending on the process
-        self.shift: list[int] = []  # Time shifted value can be 1 or more depending on the process
-        self.priority:int = 0
         self.waiting_time:int = 0
         self.turnaround_time:int = 0
+        self.time_executed: list[int] = []  # Time when executed can be 1 or many depending on the process
+        self.shift: list[int] = []  # Time shifted value can be 1 or more depending on the process
 
     def __str__(self) -> str:
             return f"""ID: {self.id}
@@ -44,6 +43,8 @@ CPU Burst: {self.cpu_burst}
 CPU Burst Needed: {self.cpu_burst_needed}
 Waiting Time: {self.waiting_time}
 Turnaround Time: {self.turnaround_time}
+Time Executed: {self.time_executed}
+Time Shifted: {self.shift}
 """
 
        
@@ -53,13 +54,48 @@ Turnaround Time: {self.turnaround_time}
 
 class algo_printer:
     util = algo_util()
-    def gant_printer(self,gant_string):
-        final_string: str = ""
-        current_char: str = None
+    def gant_printer(self, gant_string):
+        final_string = ""
+        str_list = [char for char in gant_string]
+        current_char = None
         current_char_count = 0
-       
 
-        
+        for char in str_list:
+            if current_char == char:
+                current_char_count += 1
+            else:
+                if current_char:
+                    final_string += "|"
+                    for x in range(current_char_count):
+                        if x == current_char_count // 2:
+                            if current_char == "-":
+                                final_string += "@"
+                            else:
+                                final_string += current_char
+                        else:
+                            if current_char == "-":
+                                final_string += "@"
+                            else:
+                                final_string += "-"
+                    current_char_count = 0  # Reset count for the new character
+
+                current_char = char
+                current_char_count = 1
+
+        # Append the remaining characters after the loop ends
+        if current_char:
+            final_string += "|"
+            for x in range(current_char_count):
+                if x == current_char_count // 2:
+                    final_string += current_char
+                else:
+                    if current_char == "-":
+                        final_string += " "
+                    else:
+                        final_string += "-"
+
+
+        final_string += "|"
         print(final_string)
 
     def turnaround_printer(self,task_list:list[Task]):
@@ -114,13 +150,10 @@ class Algorithm():
                 gant_string += "-"
 
             counter += 1  # Increment counter regardless of tasks
-            
-        print(gant_string)
-        self.printer.gant_printer(finished_tasks)
-        self.printer.turnaround_printer(finished_tasks)
-        self.printer.waiting_time_printer(finished_tasks)
-        #revert cpu burst needed
 
+        self.printer.gant_printer(gant_string)
+        self.printer.turnaround_printer(task_list)
+        self.printer.waiting_time_printer(task_list)
         self.revert_cpu_burst(task_list)
     def spf(self, task_list):
         counter = 0
@@ -162,18 +195,15 @@ class Algorithm():
             counter += 1  # Increment counter regardless of tasks
 
 
-        print(gant_string)
-        self.printer.gant_printer(finished_tasks)
-        self.printer.turnaround_printer(finished_tasks)
-        self.printer.waiting_time_printer(finished_tasks)
-
-        #revert cpu burst needed
+        self.printer.gant_printer(gant_string)
+        self.printer.turnaround_printer(task_list)
+        self.printer.waiting_time_printer(task_list)
         self.revert_cpu_burst(task_list)
     def srtf(self, task_list):
         counter = 0
         queue = []
         gant_string = ""
-        current_task = None
+        current_task:Task = None
         finished_tasks = []
         task_list = sorted(task_list, key=lambda x: x.arrival_time)
         copy_task_list = task_list[:]
@@ -184,7 +214,7 @@ class Algorithm():
 
             if queue:
                 queue = sorted(queue, key=lambda x: x.cpu_burst_needed)
-                c_shortest = queue[0]
+                c_shortest: Task = queue[0]
 
                 if not current_task or c_shortest.cpu_burst_needed < current_task.cpu_burst_needed:
                     if current_task:
@@ -192,6 +222,10 @@ class Algorithm():
                         queue.append(current_task)
                     current_task = queue.pop(0)
                     current_task.time_executed.append(counter)
+                    if len(current_task.time_executed) > 1:
+                        current_task.waiting_time = current_task.waiting_time + (current_task.time_executed[-1] - current_task.shift[0])
+                    else:
+                        current_task.waiting_time = self.util.task_waiting_time(counter,current_task.arrival_time)
 
             if current_task:
                 gant_string += current_task.id
@@ -208,8 +242,10 @@ class Algorithm():
 
             counter += 1
 
-        print(gant_string)
-        self.printer.gant_printer(finished_tasks)
+        self.printer.gant_printer(gant_string)
+        self.printer.turnaround_printer(task_list)
+        self.printer.waiting_time_printer(task_list)
+        self.revert_cpu_burst(task_list)
 
         
     def revert_cpu_burst(self,task_list: list[Task]):
@@ -218,7 +254,6 @@ class Algorithm():
             
     def add_to_queue(self, task_list, counter, queue):
         if task_list[0].arrival_time == counter:
-            task_list[0].time_executed.append(counter)
             queue.append(task_list[0])
             task_list.pop(0)
         return queue, task_list 
@@ -242,12 +277,13 @@ class Algorithm():
 if __name__ == '__main__':
     tasks = []
     
-    task1 = Task('A', 0, 4)
-    task2 = Task('B', 6, 2)
-    task3 = Task('C', 8, 5)
-    task4 = Task('D', 9, 15)
+    task1 = Task('A', 0, 8)
+    task2 = Task('B', 3, 4)
+    task3 = Task('C', 4, 5)
+    task4 = Task('D', 6, 3)
     task5 = Task('E', 10, 2)
     tasks.extend([task1,task2,task3,task4,task5])
     algo = Algorithm()
+    algo_u = algo_printer()
 
-    algo.srtf(tasks)
+    algo.fcfs(tasks)
