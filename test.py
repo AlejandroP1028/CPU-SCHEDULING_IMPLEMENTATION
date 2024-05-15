b5f6c120c4aa7b7002b009b670640183bc0fd2c4
eyronnnn <128238103+eyronnnn@users.noreply.github.com>
2024-05-15 21:16:28 +0800

    added unique id and multiple characters handling

--- test.py
+++ test.py
@@ -4,40 +4,53 @@
 
 # bug 2: if id of task is more than 1 the printer would go wild
 # solution: limit id of task to 1 character
-# status: not yet resolved
+# status: resolved 
 class algo_util:
-    def get_total_burst(self ,task_list):
+    def get_total_burst(self, task_list):
         total_burst = 0
         for task in task_list:
             total_burst += task.cpu_burst
         return total_burst
 
-    def task_waiting_time(self, time_executed ,arrival_time):
+    def task_waiting_time(self, time_executed, arrival_time):
         # waiting time = time executed - arrival time
         return time_executed - arrival_time
 
-    def task_turnaround_time(self ,time_of_completion ,arrival_time):
+    def task_turnaround_time(self, time_of_completion, arrival_time):
         # time of completion - arrival time
         return time_of_completion - arrival_time
 
-    def avg(self ,list):
+    def avg(self, lst):
         num = 0
-        for i in list:
+        for i in lst:
             num += i
-        return num /len(list)
+        return num / len(lst)
+
 
 class Task:
     tasks = []
 
     def __init__(self, id: str, arrival_time: int, cpu_burst: int) -> None:
+        if len(id) != 1:
+            raise ValueError("Task ID must be a single character.")
+        if self.check_id_exists(id):
+            raise ValueError("Task ID must be unique.")
         self.id = id
         self.arrival_time = arrival_time
         self.cpu_burst = cpu_burst
         self.cpu_burst_needed = cpu_burst
-        self.waiting_time :int = 0
-        self.turnaround_time :int = 0
+        self.waiting_time: int = 0
+        self.turnaround_time: int = 0
         self.time_executed: list[int] = []  # Time when executed can be 1 or many depending on the process
         self.shift: list[int] = []  # Time shifted value can be 1 or more depending on the process
+        Task.tasks.append(self)
+
+    @staticmethod
+    def check_id_exists(id: str) -> bool:
+        for task in Task.tasks:
+            if task.id == id:
+                return True
+        return False
 
     def __str__(self) -> str:
         return f"""ID: {self.id}
@@ -50,13 +63,13 @@ Time Executed: {self.time_executed}
 Time Shifted: {self.shift}
 """
 
-
-
     def __repr__(self) -> str:
         return self.__str__()
 
+
 class algo_printer:
     util = algo_util()
+
     def gant_printer(self, gant_string):
         final_string = ""
         str_list = [char for char in gant_string]
@@ -97,11 +110,10 @@ class algo_printer:
                     else:
                         final_string += "-"
 
-
         final_string += "|"
         print(final_string)
 
-    def turnaround_printer(self ,task_list :list[Task]):
+    def turnaround_printer(self, task_list: list[Task]):
         turnaround_list = []
         turnaround_str = "Turnaround Time:\n"
         for task in task_list:
@@ -111,7 +123,7 @@ class algo_printer:
         turnaround_str += f"Average TA: {self.util.avg(turnaround_list)}ms"
         print(turnaround_str)
 
-    def waiting_time_printer(self ,task_list :list[Task]):
+    def waiting_time_printer(self, task_list: list[Task]):
         waiting_time = []
         waiting_time_str = "Waiting Time:\n"
         for task in task_list:
@@ -120,7 +132,9 @@ class algo_printer:
 
         waiting_time_str += f"Average WT: {self.util.avg(waiting_time)}ms"
         print(waiting_time_str)
-class Algorithm():
+
+
+class Algorithm:
     printer = algo_printer()
     util = algo_util()
 
@@ -128,7 +142,7 @@ class Algorithm():
         counter = 0
         queue = []
         gant_string: str = ""
-        current_task :Task = None
+        current_task: Task = None
         finished_tasks = []
         task_list = sorted(task_list, key=lambda x: x.arrival_time)
         copy_task_list = task_list[:]
@@ -141,10 +155,9 @@ class Algorithm():
                 if self.check_first_in_q(queue, counter) and not current_task:
                     current_task = queue.pop(0)
                     current_task.time_executed.append(counter)
-                    current_task.waiting_time = self.util.task_waiting_time(counter ,current_task.arrival_time)
+                    current_task.waiting_time = self.util.task_waiting_time(counter, current_task.arrival_time)
 
             if current_task:
-
                 gant_string += current_task.id
                 current_task.cpu_burst_needed -= 1
                 current_task, finished_tasks = self.process_finished_task(current_task, finished_tasks, counter)
@@ -158,20 +171,21 @@ class Algorithm():
         self.printer.turnaround_printer(task_list)
         self.printer.waiting_time_printer(task_list)
         self.revert_cpu_burst(task_list)
+
     def spf(self, task_list):
         counter = 0
         queue = []
         gant_string: str = ""
-        current_task :Task = None
+        current_task: Task = None
         finished_tasks = []
-        task_list :list[Task] = sorted(task_list, key=lambda x: x.arrival_time)
+        task_list: list[Task] = sorted(task_list, key=lambda x: x.arrival_time)
         copy_task_list = task_list[:]
 
         while len(finished_tasks) != len(task_list):
 
             # if there are still tasks in task list append it to queue if the first index of the task_list.arrival time == is equal to the counter
-            if  copy_task_list:
-                queue, copy_task_list = self.add_to_queue(copy_task_list ,counter, queue)
+            if copy_task_list:
+                queue, copy_task_list = self.add_to_queue(copy_task_list, counter, queue)
 
             # if queue has tasks
             if queue:
@@ -184,8 +198,7 @@ class Algorithm():
                     # current task will be the first in q then change task attributes
                     current_task = queue.pop(0)
                     current_task.time_executed.append(counter)
-                    current_task.waiting_time = self.util.task_waiting_time(counter ,current_task.arrival_time)
-
+                    current_task.waiting_time = self.util.task_waiting_time(counter, current_task.arrival_time)
 
             if current_task:
                 gant_string += current_task.id
@@ -197,16 +210,16 @@ class Algorithm():
 
             counter += 1  # Increment counter regardless of tasks
 
-
         self.printer.gant_printer(gant_string)
         self.printer.turnaround_printer(task_list)
         self.printer.waiting_time_printer(task_list)
         self.revert_cpu_burst(task_list)
+
     def srtf(self, task_list):
         counter = 0
         queue = []
         gant_string = ""
-        current_task :Task = None
+        current_task: Task = None
         finished_tasks = []
         task_list = sorted(task_list, key=lambda x: x.arrival_time)
         copy_task_list = task_list[:]
@@ -229,7 +242,7 @@ class Algorithm():
                         waiting_time_y = current_task.time_executed[-1] - current_task.shift[-1]
                         current_task.waiting_time = current_task.waiting_time + waiting_time_y
                     else:
-                        current_task.waiting_time = self.util.task_waiting_time(counter ,current_task.arrival_time)
+                        current_task.waiting_time = self.util.task_waiting_time(counter, current_task.arrival_time)
 
             if current_task:
                 gant_string += current_task.id
@@ -237,7 +250,8 @@ class Algorithm():
 
                 if current_task.cpu_burst_needed <= 0:
                     current_task.shift.append(counter + 1)
-                    current_task.turnaround_time = self.util.task_turnaround_time(counter + 1, current_task.arrival_time)
+                    current_task.turnaround_time = self.util.task_turnaround_time(counter + 1,
+                                                                                  current_task.arrival_time)
                     finished_tasks.append(current_task)
                     current_task = None
 
@@ -255,7 +269,7 @@ class Algorithm():
         self.printer.waiting_time_printer(task_list)
         self.revert_cpu_burst(task_list)
 
-    def revert_cpu_burst(self ,task_list: list[Task]):
+    def revert_cpu_burst(self, task_list: list[Task]):
         for task in task_list:
             task.cpu_burst_needed = task.cpu_burst
 
@@ -266,6 +280,7 @@ class Algorithm():
         if added_tasks:
             queue.extend(added_tasks)
         return queue, task_list
+
     def check_first_in_q(self, queue: list[Task], counter):
         if queue and queue[0].arrival_time <= counter:
             return True
@@ -274,7 +289,7 @@ class Algorithm():
     def process_finished_task(self, current_task: Task, finished_tasks: list[Task], counter):
         if current_task.cpu_burst_needed == 0:
             current_task.shift.append(counter + 1)
-            current_task.turnaround_time = self.util.task_turnaround_time(counter +1 ,current_task.arrival_time)
+            current_task.turnaround_time = self.util.task_turnaround_time(counter + 1, current_task.arrival_time)
             finished_tasks.append(current_task)
             current_task = None
 
@@ -288,7 +303,16 @@ if __name__ == '__main__':
 
     num_tasks = int(input("Enter the number of tasks: "))
     for i in range(num_tasks):
-        id = input(f"Enter task ID for task {i + 1}: ")
+        while True:
+            id = input(f"Enter task ID for task {i + 1}: ")
+            if len(id) != 1:
+                print("Task ID must be a single character. Please try again.")
+                continue
+            elif Task.check_id_exists(id):
+                print("Task ID must be unique. Please try again.")
+                continue
+            break
+
         arrival_time = int(input(f"Enter arrival time for task {id}: "))
         cpu_burst = int(input(f"Enter CPU burst for task {id}: "))
         task = Task(id, arrival_time, cpu_burst)
@@ -308,4 +332,5 @@ if __name__ == '__main__':
             break
         else:
             print("Invalid choice. Please try again.")
+
 
