a10bfb1b24c12428d9e0a8b11d7127c2b9ac0b59
Alejandro Prado <alejandroprado885@gmail.com>
2024-05-24 16:04:02 +0800

    added card and addTask components

---
+++ os_js/src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="['rounded-lg shadow', computedClassesMain]">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+  props: {
+    text: String,
+  },
+  computed: {
+    computedClassesMain() {
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add any scoped styles if needed */
+</style>
